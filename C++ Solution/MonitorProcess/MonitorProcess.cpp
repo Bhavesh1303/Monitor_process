3f5a5ad04bbaed80a4ea40321752dbc97f403bc7
Bhavesh Sharma <bhaveshs13399@gmail.com>
2021-03-07 01:59:50 +0530

    modify the c++ solution with the new approach of maiking list of process

--- C++ Solution/MonitorProcess/MonitorProcess.cpp
+++ C++ Solution/MonitorProcess/MonitorProcess.cpp
@@ -5,27 +5,47 @@
 #include <fstream> 
 #include <algorithm>
 #include <ctime>
+#include <iostream>  // std::cout
+#include <vector>  // std::vector, like List<T> in C# (or ArrayList<T> in Java)
+#include <Windows.h>
+#include <Tlhelp32.h>
+#include <iterator>
+
 using namespace std;
 
 ofstream filestream;
 
-void PrintProcessNameAndID(DWORD processID)
+std::vector<DWORD> EnumProcs()
 {
+    std::vector<DWORD> pids;
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (snapshot != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32 pe32 = { sizeof(PROCESSENTRY32) };
+        if (Process32First(snapshot, &pe32))
+        {
+            do
+            {
+                pids.push_back(pe32.th32ProcessID);
+            } while (Process32Next(snapshot, &pe32));
+        }
+        CloseHandle(snapshot);
+    }
+    return pids;
+}
+
+void PrintProcessNameAndID(DWORD processID, const wchar_t AppStatus[])
+{
+    cout << "Process ID:" << processID << "\n";
+    
     TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
-    TCHAR AppStatus[MAX_PATH] = {0};
+    
     FILETIME createTime;
     FILETIME exitTime;
     FILETIME kernelTime;
     FILETIME userTime;
     SYSTEMTIME AppStartTime;
-    DWORD exitCode;
-    //FILETIME now;
-    //SYSTEMTIME systemTime;
-
-    //GetSystemTimeAsFileTime(&now);
-    //FileTimeToSystemTime(&now, &systemTime);
-
-    // Get a handle to the process.
+    
 
     HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
         PROCESS_VM_READ,
@@ -45,30 +65,21 @@ void PrintProcessNameAndID(DWORD processID)
                 sizeof(szProcessName) / sizeof(TCHAR));
         }
         std::string strtmp(&szProcessName[0], &szProcessName[30]);
+        wstring ws(AppStatus);
+        string str(ws.begin(), ws.end());
 
-        
         
         if (GetProcessTimes(hProcess, &createTime, &exitTime, &kernelTime, &userTime) != -1)
         {
             if (FileTimeToSystemTime(&createTime, &AppStartTime) != -1) {
-                if (GetExitCodeProcess(hProcess, &exitCode) == 0) {
+                //if (_tcscmp(szProcessName, _T("<unknown>"))) {
                     // Print the process name and identifier.
-                    _tprintf(TEXT("%s  %u  %02d:%02d:%02d:%03d  STOP\n"), szProcessName, processID, AppStartTime.wHour, AppStartTime.wMinute, AppStartTime.wSecond, AppStartTime.wMilliseconds);//, AppStatus
-                    filestream << strtmp << "\t" << processID << "\t" << AppStartTime.wHour << ":" << AppStartTime.wMinute << ":" << AppStartTime.wSecond << ":" << AppStartTime.wMilliseconds << "\t" << " STOP " << "\n";
-                }
-                else {
-                    // Print the process name and identifier.
-                    _tprintf(TEXT("%s  %u  %02d:%02d:%02d:%03d  START\n"), szProcessName, processID, AppStartTime.wHour, AppStartTime.wMinute, AppStartTime.wSecond, AppStartTime.wMilliseconds);//, AppStatus
-                    filestream << strtmp << "\t" << processID << "\t" << AppStartTime.wHour << ":" << AppStartTime.wMinute << ":" << AppStartTime.wSecond << ":" << AppStartTime.wMilliseconds << "\t" << " START " << "\n";
-                }
+                    _tprintf(TEXT("%s  %u  %02d:%02d:%02d:%03d %s\n"), szProcessName, processID, AppStartTime.wHour, AppStartTime.wMinute, AppStartTime.wSecond, AppStartTime.wMilliseconds, AppStatus);
+                    filestream << strtmp << "\t" << processID << "\t" << AppStartTime.wHour << ":" << AppStartTime.wMinute << ":" << AppStartTime.wSecond << ":" << AppStartTime.wMilliseconds << "\t" << str << "\n";
+                //}
             }
         }
     }
-    
-    
-    
-    //filestream << strtmp << "\t" << processID << "\n";
-
     // Release the handle to the process.
     CloseHandle(hProcess);
 }
@@ -76,7 +87,6 @@ void PrintProcessNameAndID(DWORD processID)
 int main(void)
 {
     // Get the list of process identifiers.
-
     DWORD aProcesses[1024], cbNeeded, cProcesses;
     unsigned int i;
 
@@ -85,27 +95,37 @@ int main(void)
         return 1;
     }
 
-
     // Calculate how many process identifiers were returned.
-
     cProcesses = cbNeeded / sizeof(DWORD);
 
-    // Print the name and process identifier for each process.
-
+    std::vector<DWORD> pids1 = EnumProcs();
+    sort(pids1.begin(), pids1.end());
+    
+    std::vector<DWORD> pids2 = EnumProcs();
+    sort(pids2.begin(), pids2.end());
+    
     
     filestream.open("result.txt");
     filestream << "Process Name" << "\t\t\t" << "PID" << "\t" << "Time" << "\t\t" << "Status" << "\n";
     while(true){
-        for (i = 0; i < cProcesses; i++)
-        {
-            if (aProcesses[i] != 0)
-            {
-                PrintProcessNameAndID(aProcesses[i]);
-            }
-        }
-        filestream.close();
-        printf("\n");
-        Sleep(2000);
+        std::cout << " ----------------------------------------------------------------------- \n";
+        pids2 = EnumProcs();
+        sort(pids2.begin(), pids2.end());
+
+        std::vector<DWORD> create_diff;
+        std::set_difference(pids1.begin(), pids1.end(), pids2.begin(), pids2.end(), std::inserter(create_diff, create_diff.begin()));
+        std::cout << create_diff.size() << " Process Created \n";
+        for (DWORD i : create_diff) PrintProcessNameAndID( i, L"Start");
+
+        std::vector<DWORD> terminate_diff;
+        std::set_difference(pids2.begin(), pids2.end(), pids1.begin(), pids1.end(), std::inserter(terminate_diff, terminate_diff.begin()));
+        std::cout << terminate_diff.size() << " Process Ended \n";
+        for (DWORD i : terminate_diff) PrintProcessNameAndID( i, L"Stop");
+        
+        Sleep(1000);
+        pids1 = pids2;
+        sort(pids1.begin(), pids1.end());
     }
+    filestream.close();
     return 0;
 }
